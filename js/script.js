dced9cca3554e74d78c7016c720a8600f2107ff5
Asutosh Palai <asupalai@gmail.com>
2015-03-28 16:17:22 +0530

    Added auto selection of first category

--- js/script.js
+++ js/script.js
@@ -8,14 +8,25 @@ $container.isotope({
 
 
 $('.category').click(function(event){
-  var cat = $(this).data('cat');
-  console.log(cat);
+  var element = $(this);
+  var cat = element.data('cat');
   $container.isotope({ filter:'.' + cat});
+  element.addClass("active");
+  document.activecat.removeClass('active');
+  document.activecat = element;
    
 });
 $container.imagesLoaded( function() {
     $container.isotope('layout');
 
 } );
+
+
+document.activecat = $('.category').first();
+document.activecat.addClass('active');
+var cat = document.activecat.data('cat');
+$container.isotope({ filter:'.' + cat});
+
+
 
 });
